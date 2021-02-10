b6b362ac66d27e652fda3839d07292846576655b
rizgust <rizgust.dev@gmail.com>
2021-02-10 21:09:03 +0700

    [ADD] tracer studies

--- pages/tracer.vue
+++ pages/tracer.vue
@@ -4,12 +4,20 @@
       title="Tracer Studies"
       subtitle="Tracer Studies"
       image="/uploads/about-tracer.jpg"
-    ></site-hero>
-    <main-section theme="sidebar-right">
+    ></site-hero><main-section theme="sidebar-right">
       <template v-slot:default>
-        
+        <div class="content">
+          <h3>Member List</h3>
+          <iframe
+            class="airtable-embed"
+            src="https://airtable.com/embed/shrMAijyxlZsXjSdI?backgroundColor=gray&viewControls=on"
+            frameborder="0"
+            onmousewheel=""
+            width="100%"
+            style="background: transparent; border: 1px solid #fff; height: 1400px;"
+          ></iframe>
+        </div>
       </template>
-
       <template v-slot:sidebar>
         <h3 class="subtitle is-4">
           Latest Posts
