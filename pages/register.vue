631c1878cd8ae1ad3864d5d0395c9ac330a72511
rizgust <rizgust.dev@gmail.com>
2021-03-31 13:17:15 +0700

    [ADD] register form using airtable

---
+++ pages/register.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="about-page" class="page-wrapper about-page content-page">
+    <site-hero
+      title="Register"
+      subtitle="Member Registration"
+      image="/uploads/about-tracer.jpg"
+    ></site-hero><main-section >
+      <template v-slot:default>
+        <div class="content">
+          <iframe
+            class="airtable-embed"
+            src="https://airtable.com/embed/shrsmHUAHOC9HRWVC?backgroundColor=gray&layout=card&viewControls=on"
+            frameborder="0"
+            onmousewheel=""
+            width="100%"
+            style="background: transparent; border: 1px solid #fff; height: 1950px;"
+          ></iframe>
+        </div>
+      </template>
+    </main-section>
+  </div>
+</template>
+<script>
+import QuoteCard from '~/components/cards/QuoteCard'
+export default {
+    head() {
+        return {
+            title: `About | ${this.$siteConfig.siteName}`
+        }
+    },
+    components: { QuoteCard }
+}
+</script>
