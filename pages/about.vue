92f4b248e889de4aa4af8d9943da4868b82a50fa
rizgust <rizgust.dev@gmail.com>
2021-06-20 23:23:08 +0700

    [ADD] populate content

--- pages/about.vue
+++ pages/about.vue
@@ -1,37 +1,84 @@
 <template>
   <div id="about-page" class="page-wrapper about-page content-page">
     <site-hero
-      title="About IKA Geo UB"
+      title="IKA Geofisika UB"
       subtitle="Tentang Ikatan Alumni Geofisika Universitas Brawijaya"
       image="/uploads/about-hero.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
       <template v-slot:default>
-        <div class="content">
-          <h3>Latar Belakang</h3>
-          <p>
-            latar belakang berdirinya IKA Geofisika Universitas Brawijaya
+        <div class="content setema">
+          <h1>Latar Belakang</h1>
+          <p class="justify ">
+            Geofisika Universitas Brawijaya sudah berdiri sejak 20 Agustus 1990 dalam bentuk KBM (Kelompok Bidang Minat), dibawah Jurusan Fisika, Fakultas Matematika dan Ilmu Pengetahuan Alam. Pada tahun 2010, Penyelenggaraan Minat Prodi Geofisika resmi dibentuk melalui Keputusan Rektor No. 381/SK/2010 untuk Program Studi (S1) tertanggal 31 Desember 2010. Hingga saat ini Geofisika Universitas Brawijaya telah bermetamorfosis menjadi Program Studi Teknik Geofisika dengan mengacu pada Izin Operasional atau Penyelenggaraan Program Studi S1 Geofisika, Jurusan Fisika melalui Keputusan Menteri Pendidikan dan Kebudayaan Republik Indonesia Nomor: 595/E/O/2014 tentang izin penyelenggaraan program studi pada Universitas Brawijaya Malang. Tertanggal 17 Oktober 2014 resmi menggunakan nama “Teknik Geofisika Program Sarjana” mengikuti nomenklatur yang sudah ada. 
           </p>
-          <h3>Kronologi Pembentukan</h3>
-          <p>
-            -
+          <p class="justify">
+          Dengan usia 30 tahun di tahun 2020 ini, tentunya sudah banyak alumni yang dihasilkan dan tersebar diberbagai bidang dunia kerja di bidang kebumian maupun non-kebumian. Ditengah perkembangan industri yang semakin pesat, dan tuntutan kemajuan diberbagai aspek yang semakin tinggi, para alumni merasa terpanggil untuk memberikan kontribusi kepada almamater serta masyarakat secara umum, sesuai bidang ilmu yang digeluti. Untuk itu perlu dibentuk wadah resmi yang khusus untuk alumni Geofisika Universitas Brawijaya agar dapat menjaga silaturrahmi dan menyalurkan kontribusinya secara terorganisir.
           </p>
-          <h3>Musyawarah Besar</h3>
-          <p>
-            -
-          </p>
-          <h3>Logo</h3>
-          <p>
-            -
-          </p>
-          <h3>Struktur Organisasi</h3>
-          <p>
-            -
-          </p>
-          <h3>Program Kerja</h3>
-          <p>
-            -
-          </p>
+          <h1>Kronologi Pembentukan</h1>
+          <p class="tight"><b>Agustus, 2020</b></p>
+          <p class="tight">&nbsp;&nbsp;&nbsp;&nbsp;&#9679; Meeting Virtual beberapa alumni, hasilnya, mufakat perlu dibuat organisasi IKA.</p>
+          <p class="tight">&nbsp;&nbsp;&nbsp;&nbsp;&#9679; Dibentuk Tim Pembentuk IKA.</p>
+          <p class="tight"><b>September, 2020</b></p>
+          <p class="tight">&nbsp;&nbsp;&nbsp;&nbsp;&#9679; sosialisasi dan menggalang dukungan dari seluruh alumni hingga ke civitas akademik Prodi Teknik Geofisika UB, melalui kuisioner online.</p>
+          <p class="tight"><b>Oktober, 2020</b></p>
+          <p class="tight">&nbsp;&nbsp;&nbsp;&nbsp;&#9679; <b>Tanggal 10</b>, Rekap Kuisioner online, 100% responden setuju perlu dibentuk IKA, dengan 80% setuju dengan nama IKA Geofisika UB</p>
+          <p class="tight">&nbsp;&nbsp;&nbsp;&nbsp;&#9679; <b>Tanggal 12-24</b> Sosialisasi dan Diskusi Draft AD-ART, dan Sosialisasi Calon Dewan Pembina IKA Geofisika UB</p>
+          <p class="tight">&nbsp;&nbsp;&nbsp;&nbsp;&#9679; <b>Tanggal 23-29</b> Pendaftaran Bakal Calon Ketua Umum IKA</p>
+          <p class="tight">&nbsp;&nbsp;&nbsp;&nbsp;&#9679; <b>Tanggal 30-31</b> Verifikasi dan Pengumuman Calon Ketua Umum;</p>
+          <p class="tight"><b>Nopember, 2020</b></p>
+          <p class="tight">&nbsp;&nbsp;&nbsp;&nbsp;&#9679; <b>Tanggal 01-15</b> Masa kampanye calon ketua IKA, dan Sayembara Logo IKA untuk mahasiswa Teknik Geofisika UB.</p>
+          <p class="tight">&nbsp;&nbsp;&nbsp;&nbsp;&#9679; <b>Tanggal 16-19</b> Voting pemilihan calon ketua IKA :</p>
+          <p class="tight">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>1) Sapto Hadi Wibowo (2005)</b></p>
+          <p class="tight">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>2) Wahyudi (2001)</b></p>
+          <p class="tight">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>3) Arief Rahman (2003)</b></p>
+          <p class="tight">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>4) Nurul Hikmah Kartini (2000)</b></p>
+          <p class="tight"><b>20 Nopember, 2020 - Musyawarah besar (MUBES) </b></p>
+          <h1>Struktur Tim Pembentukan IKA Geofisika UB</h1>
+          <img :src="'/Structure-mus.png'">
+          <h1>Musyawarah Besar</h1>
+          <p class="justify tight">Mubes adalah Pertemuan akbar alumni untuk meresmikan berdirinya IKA Geofisika UB diikuti oleh seluruh perwakilan alumni, dewan pembina, dosen dan mahasiswa Geofisika UB.</p>
+          <br>
+          <p class="justify tight">Hari/Tanggal : Jumat, 20 November 2020</p>
+          <p class="justify tight">Waktu: 19.30 – 21.30 WIB</p>
+          <br>
+          <p class="justify tight">Agenda Mubes : </p>
+          <p class="tight">&#9679; <b>Doa</b> dipmpin oleh Joko Wiyono (2003)</p>
+          <p class="tight">&#9679; <b>Pembukaan</b> oleh MC Alvira Galih – mahasiswa Teknik Geofisika UB, Sambutan Perwakilan Panitia oleh M Noor Alamsyah / Mas Koko (1998), dan Perwakilan Dosen Dr. Sunaryo, M.Si.</p>
+          <p class="tight">&#9679; <b>Penetapan Presidium Mubes</b> terpilih yaitu Riky Hendrawan (2004), Chaerul Firmansyah (2003) dan M. Irfan Mashudi (2004)</p>
+          <p class="tight">&#9679; <b>Pembacaan Peraturan dan Tata Tertib Mubes</b> oleh Presidium Mubes</p>
+          <p class="tight">&#9679; <b>Pengesahan berdirinya IKA</b> oleh Presidium Mubes</p>
+          <p class="tight">&#9679; <b>Pengukuhan 8 Dewan Pembina IKA</b> oleh Presidium Mubes</p>
+          <p class="tight">&#9679; <b>Pengesahan AD-ART dan Logo IKA</b></p>
+          <p class="tight">&#9679; <b>Pembacaan Hasil Voting Calon Ketum IKA</b> oleh ketua Pemilu IKA; Warto Utomo (2003)</p>
+          <p class="tight">&#9679; <b>Pelantikan Ketum IKA terpilih;</b> Arief Rahman (2003) dilantik oleh Prof. Drs. Adi Susilo, M.Si., Ph.D selaku dewan pembina.</p>
+          <p class="tight">&#9679; <b>Pemaparan Program Kerja Ketum IKA terpilih</b></p>
+          <p class="tight">&#9679; <b>Sesi Dokumentasi</b></p>
+          <h1>Logo</h1>
+          <img class="logo-full":src="'/Logo-full.png'">
+          <h1>Filosofi Logo</h1>
+          <div class="logo-def">
+            <img class="logo-def-left":src="'/Logo-toga.png'">
+            <div class="logo-def-right">
+              <h3><b>Toga</b></h3>
+              <p class="justify"><b>Wisuda merupakan simbol kelulusan seseorang yang telah menyelesaikan masa studinya, divisualisasikan melalui toga yang merepresentasikan alumnni.</b></p>
+            </div>
+          </div>
+          <div class="logo-def">
+            <img class="logo-def-left":src="'/Logo-bumi.png'">
+            <div class="logo-def-right">
+              <h3><b>Bumi</b></h3>
+              <p class="justify"><b>Bumi dan lapisannya merepresentasikan program studi teknik geofisika yang secara umum mempelajari tentang bumi melalui perspektif fisika</b></p>
+            </div>
+          </div>
+          <h1>Struktur Organisasi</h1>
+          <img :src="'/Structure.png'">
+          <h1>Program Kerja</h1>
+          <p>&#9679; Pendataan  dan Pemetaan Minat  Alumni</p>
+          <p>&#9679; Sharing Session dengan tema Kebumian, Enterpreneur, Soft Skill, dan Pengabdian kepada Masyarakat.</p>
+          <p>&#9679; Membuat Website dan Media Sosial sebagai rumah virtual IKA.</p>
+          <p>&#9679; Sebagai Fasilitator dalam MoU dan kunjungan rutin antara pihak Program Studi dengan Perusahaan</p>
+          <p>&#9679; Pengenalan Geofisika/Ilmu Kebumian ke Sekolah Menengah Atas/setara</p>
         </div>
       </template>
 
@@ -49,9 +96,43 @@ import QuoteCard from '~/components/cards/QuoteCard'
 export default {
     head() {
         return {
-            title: `About | ${this.$siteConfig.siteName}`
+            title: `Lingkup | ${this.$siteConfig.siteName}`
         }
     },
     components: { QuoteCard }
 }
 </script>
+<style scoped lang="scss">
+.setema{
+  p, h1, h3{
+    color: #000066 !important;
+  }
+}
+.justify{
+  text-align: justify;
+  text-justify: inter-word;
+}
+.tight{
+  margin-bottom: 4px !important;
+}
+.logo-full{
+  max-width: 500px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+.logo-def{
+  display: flex;
+  .logo-def-left{
+    margin-left: 10%;
+  }
+  .logo-def-right{
+    margin-right: 20%;
+    padding-top: 35px;
+  }
+  img{
+    max-width: 250px;
+  }
+}
+</style>
