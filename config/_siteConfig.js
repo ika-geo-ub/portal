be746b78b48df77b56c06b4a2b46902e62a15b9f
rizgust <rizgust.dev@gmail.com>
2021-06-20 23:22:39 +0700

    [ADD] change background image

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -11,7 +11,7 @@ import theCMS from '../cms/netlify/cms'
 export default {
     siteName: 'IKA Geofisika UB',
     tagline: 'Ikatan Alumni Geofisika Universitas Brawijaya',
-    featureImage: '/uploads/home-hero.jpg',
+    featureImage: '/uploads/about-hero.jpg',
     logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
     googleAnalytics: {
         on: true,
